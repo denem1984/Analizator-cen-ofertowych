ff67fbf03bfdc6cf50c62a45aec0b635342e41ca
denem1984 <91011065+denem1984@users.noreply.github.com>
2026-08-23 22:00:08 +0200

    Switch commercial parser wrapper to corrected v03

--- parser-commercial-v01.js
+++ parser-commercial-v01.js
@@ -1,3 +1,3 @@
-// Compatibility wrapper: commercial search parser v0.6 uses an isolated implementation.
+// Compatibility wrapper: commercial search parser uses the isolated v03 implementation.
 // Residential/mieszkanie parsers are not touched by this module.
-module.exports=require('./parser-commercial-v02');
+module.exports=require('./parser-commercial-v03');
